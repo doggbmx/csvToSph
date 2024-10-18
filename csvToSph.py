cf446d46eec36032afbb356e3a872fb156277b21
doggbmx <manuelorticosta@gmail.com>
2024-10-17 21:36:58 -0300

    first commit

---
+++ csvToSph.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Cargar el archivo CSV
+csv_file_path = 'locales_de_coleta_de_amostras.csv'
+data = pd.read_csv(csv_file_path)
+
+# Filtrar las columnas necesarias
+coordinates = data[['Local de coleta', 'Coordenadas', 'Unnamed: 5']].dropna()
+print(coordinates)
+# Crear el contenido del archivo .sph
+sph_content = ""
+for index, row in coordinates.iterrows():
+    local, lat, lon = row['Local de coleta'], row['Coordenadas'], row['Unnamed: 5']
+    sph_content += f"{local}: {lat}, {lon}\n"
+
+# Guardar el .sph
+sph_file_path = './sph/locales_de_coleta_de_amostras.sph'
+with open(sph_file_path, 'w') as file:
+    file.write(sph_content)
+
+print(f"Archivo .sph generado en: {sph_file_path}")
